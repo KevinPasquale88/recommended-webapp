97f953923c5ce9d6ba60cf118dcf46dda04084f8
Kevin <robertopasquale1988@gmail.com>
2022-12-06 01:34:17 +0100

    add charts

--- src/main/java/it/sysagent/recommended/recommendedwebapp/repository/RepositoryUsers.java
+++
@@ -1,9 +0,0 @@
-package it.sysagent.recommended.recommendedwebapp.repository;
-
-import it.sysagent.recommended.recommendedwebapp.entity.UsersEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RepositoryUsers extends JpaRepository<UsersEntity,Long> {
-}
--- src/main/java/it/sysagent/recommended/recommendedwebapp/util/Descriptors.java
+++
@@ -1,6 +0,0 @@
-package it.sysagent.recommended.recommendedwebapp.util;
-
-public interface Descriptors {
-
-    public static final String[] scale = {"Molto Negativo","Negativo", "Neutro", "Positivo", "Molto Positvo"};
-}
